98f385d07d759908db458ccbaf502002b0af4d2a
kish67 <136743077+kish67@users.noreply.github.com>
2026-02-21 14:26:43 +0530

    feat: Add public and private subnets in subnet.tf

---
+++ dev/subnet.tf
@@ -0,0 +1,13 @@
+resource "google_compute_subnetwork" "public_subnet" {
+  name          = "dev-public-subnet"
+  ip_cidr_range = "10.0.1.0/24"
+  region        = "us-central1"
+  network       = google_compute_network.dev_vpc.id
+}
+
+resource "google_compute_subnetwork" "private_subnet" {
+  name          = "dev-private-subnet"
+  ip_cidr_range = "10.0.2.0/24"
+  region        = "us-central1"
+  network       = google_compute_network.dev_vpc.id
+}
