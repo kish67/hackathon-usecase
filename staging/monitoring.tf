c67018572f84d5d5596b3d5da141347e6231a8d0
kish67 <136743077+kish67@users.noreply.github.com>
2026-02-21 15:36:33 +0530

    Update monitoring.tf

--- staging/monitoring.tf
+++ staging/monitoring.tf
@@ -6,13 +6,41 @@ resource "google_monitoring_dashboard" "staging_dashboard" {
     "columns": 2,
     "widgets": [
       {
-        "title": "CPU Usage",
+        "title": "Application Frontend CPU Usage (Staging)",
         "xyChart": {
           "dataSets": [
             {
               "timeSeriesQuery": {
                 "timeSeriesFilter": {
-                  "filter": "metric.type=\"compute.googleapis.com/instance/cpu/utilization\""
+                  "filter": "metric.type=\"kubernetes.io/container/cpu/utilization\" AND resource.type=\"k8s_container\" AND resource.label.\"container_name\"=\"applicationfrontend\" AND resource.label.\"cluster_name\"=\"staging-gke-cluster\""
+                }
+              }
+            }
+          ]
+        }
+      },
+      {
+        "title": "Patient Frontend CPU Usage (Staging)",
+        "xyChart": {
+          "dataSets": [
+            {
+              "timeSeriesQuery": {
+                "timeSeriesFilter": {
+                  "filter": "metric.type=\"kubernetes.io/container/cpu/utilization\" AND resource.type=\"k8s_container\" AND resource.label.\"container_name\"=\"patientfrontend\" AND resource.label.\"cluster_name\"=\"staging-gke-cluster\""
+                }
+              }
+            }
+          ]
+        }
+      },
+      {
+        "title": "Order Backend CPU Usage (Staging)",
+        "xyChart": {
+          "dataSets": [
+            {
+              "timeSeriesQuery": {
+                "timeSeriesFilter": {
+                  "filter": "metric.type=\"kubernetes.io/container/cpu/utilization\" AND resource.type=\"k8s_container\" AND resource.label.\"container_name\"=\"orderbackend\" AND resource.label.\"cluster_name\"=\"staging-gke-cluster\""
                 }
               }
             }
