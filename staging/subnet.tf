54ced668979cc9e774e1974375a061a6318c4094
kish67 <136743077+kish67@users.noreply.github.com>
2026-02-21 15:49:24 +0530

    Update subnet.tf

--- staging/subnet.tf
+++ staging/subnet.tf
@@ -3,14 +3,14 @@ resource "google_compute_subnetwork" "public_subnet_a" {
   name          = "staging-public-subnet-a"
   ip_cidr_range = "10.10.1.0/24"
   region        = var.region
-  network       = google_compute_network.prod_vpc.id
+  network       = google_compute_network.staging_vpc.id
 }
 
 resource "google_compute_subnetwork" "public_subnet_b" {
   name          = "staging-public-subnet-b"
   ip_cidr_range = "10.10.2.0/24"
   region        = var.region
-  network       = google_compute_network.prod_vpc.id
+  network       = google_compute_network.staging_vpc.id
 }
 
 # Private Subnets (two AZs)
@@ -18,12 +18,12 @@ resource "google_compute_subnetwork" "private_subnet_a" {
   name          = "staging-private-subnet-a"
   ip_cidr_range = "10.10.3.0/24"
   region        = var.region
-  network       = google_compute_network.prod_vpc.id
+  network       = google_compute_network.staging_vpc.id
 }
 
 resource "google_compute_subnetwork" "private_subnet_b" {
   name          = "staging-private-subnet-b"
   ip_cidr_range = "10.10.4.0/24"
   region        = var.region
-  network       = google_compute_network.prod_vpc.id
+  network       = google_compute_network.staging_vpc.id
 }
