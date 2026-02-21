f463608be26dec3fdc46022be641054dcf1ce017
kish67 <136743077+kish67@users.noreply.github.com>
2026-02-21 15:48:48 +0530

    Update subnet.tf

--- dev/subnet.tf
+++ dev/subnet.tf
@@ -1,29 +1,29 @@
 # Public Subnets (two AZs)
 resource "google_compute_subnetwork" "public_subnet_a" {
-  name          = "prod-public-subnet-a"
+  name          = "dev-public-subnet-a"
   ip_cidr_range = "10.10.1.0/24"
   region        = var.region
-  network       = google_compute_network.prod_vpc.id
+  network       = google_compute_network.dev_vpc.id
 }
 
 resource "google_compute_subnetwork" "public_subnet_b" {
-  name          = "prod-public-subnet-b"
+  name          = "dev-public-subnet-b"
   ip_cidr_range = "10.10.2.0/24"
   region        = var.region
-  network       = google_compute_network.prod_vpc.id
+  network       = google_compute_network.dev_vpc.id
 }
 
 # Private Subnets (two AZs)
 resource "google_compute_subnetwork" "private_subnet_a" {
-  name          = "prod-private-subnet-a"
+  name          = "dev-private-subnet-a"
   ip_cidr_range = "10.10.3.0/24"
   region        = var.region
-  network       = google_compute_network.prod_vpc.id
+  network       = google_compute_network.dev_vpc.id
 }
 
 resource "google_compute_subnetwork" "private_subnet_b" {
-  name          = "prod-private-subnet-b"
+  name          = "dev-private-subnet-b"
   ip_cidr_range = "10.10.4.0/24"
   region        = var.region
-  network       = google_compute_network.prod_vpc.id
+  network       = google_compute_network.dev_vpc.id
 }
